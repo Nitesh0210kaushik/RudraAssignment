43c5cc0cd5f51482c69f551772c048defa7fb377
Nitesh Kaushik <nitesh0210kaushik@gmail.com>
2023-09-12 10:07:35 +0530

    final

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,30 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//     userName: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//         trim: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         minlength: 6,
-//     },
-
-
-// });
-
-// const UserModel = mongoose.model('User', userSchema);
-
-// module.exports = UserModel;
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
